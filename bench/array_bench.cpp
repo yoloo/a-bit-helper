3d41e8ff65942d90b79cf15d0498e4c40d2d9755
lecky <yongle0624@126.com>
2020-05-20 19:42:28 +0800

    std::array benchmark

---
+++ bench/array_bench.cpp
@@ -0,0 +1,54 @@
+//
+// Created by zhaoyongle on 2020/5/20.
+//
+
+#include "benchmark/benchmark.h"
+
+constexpr int len = 6;
+
+static void bench_array_operator(benchmark::State& state)
+{
+    std::array<int, len> arr;
+    constexpr int i = 1;
+    for (auto _: state) {
+        arr[0] = i;
+        arr[1] = i + 1;
+        arr[2] = i + 2;
+        arr[3] = i + 3;
+        arr[4] = i + 4;
+        arr[5] = i + 5;
+    }
+}
+BENCHMARK(bench_array_operator);
+
+static void bench_array_at(benchmark::State& state)
+{
+    std::array<int, len> arr;
+    constexpr int i = 1;
+    for (auto _: state) {
+        arr.at(0) = i;
+        arr.at(1) = i + 1;
+        arr.at(2) = i + 2;
+        arr.at(3) = i + 3;
+        arr.at(4) = i + 4;
+        arr.at(5) = i + 5;
+    }
+}
+BENCHMARK(bench_array_at);
+
+static void bench_array_get(benchmark::State& state)
+{
+    std::array<int, len> arr;
+    constexpr int i = 1;
+    for (auto _: state) {
+        std::get<0>(arr) = i;
+        std::get<1>(arr) = i + 1;
+        std::get<2>(arr) = i + 2;
+        std::get<3>(arr) = i + 3;
+        std::get<4>(arr) = i + 4;
+        std::get<5>(arr) = i + 5;
+    }
+}
+BENCHMARK(bench_array_get)->Iterations(1000000000);
+
+BENCHMARK_MAIN();
--- bench/leetcode_bench.cpp
+++ bench/leetcode_bench.cpp
@@ -9,12 +9,12 @@ static void BM_Array_twoSum1(benchmark::State& state) {
     for (auto _ : state)
         LeetCode::Array::TwoSum::one({2, 7, 11, 15}, 9);
 }
-BENCHMARK(BM_Array_twoSum1)->Iterations(1000);
+BENCHMARK(BM_Array_twoSum1);
 
 static void BM_Array_twoSum2(benchmark::State& state) {
     for (auto _ : state)
         LeetCode::Array::TwoSum::two({2, 7, 11, 15}, 9);
 }
-BENCHMARK(BM_Array_twoSum2)->Iterations(1000);
+BENCHMARK(BM_Array_twoSum2);
 
 BENCHMARK_MAIN();
