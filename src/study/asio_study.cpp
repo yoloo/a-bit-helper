0f417eead681288f2693d7f99dbbb0e5a471c872
lecky <yongle0624@126.com>
2020-06-24 18:35:38 +0800

    sync/async timer

--- src/study/asio_study.cpp
+++ src/study/asio_study.cpp
@@ -1,14 +1,33 @@
 #include <iostream>
-
 #include <asio.hpp>
 
+namespace SyncTimer {
+    void test() {
+        asio::io_context io;
+
+        asio::steady_timer t(io, asio::chrono::seconds(5));
+        t.wait();
+
+        std::cout << "Hello, I'm sync timer" << std::endl;
+    }
+}
+
+namespace AsyncTimer {
+    void test() {
+        asio::io_context io;
+
+        asio::steady_timer t(io, asio::chrono::seconds(5));
+        t.async_wait([](const asio::error_code& err) {
+            std::cout << "Hello, I'm async timer" << std::endl;
+        });
+
+        io.run();
+    }
+}
+
 int main() {
-    asio::io_context io;
-
-    asio::steady_timer tm(io, asio::chrono::seconds(5));
-    tm.wait();
-
-    std::cout << "Hello, standalone asio!" << std::endl;
+    //SyncTimer::test();
+    AsyncTimer::test();
 
     return 0;
 }
