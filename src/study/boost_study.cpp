e6bfc365884623439b4f095390386e398a9477a2
lecky <yongle0624@126.com>
2020-07-24 14:33:38 +0800

    boost modify

--- src/study/boost_study.cpp
+++ src/study/boost_study.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 
-#include <boost/timer.hpp>
+#include <boost/date_time/gregorian/gregorian.hpp>
 
 int main() {
-    boost::timer tm;
+    boost::gregorian::date d{2020, 07, 24};
+    std::cout << d.year() << '\n';
+    std::cout << d.month() << '\n';
+    std::cout << d.day() << '\n';
+    std::cout << d.day_of_week() << '\n';
+    std::cout << d.end_of_month() << '\n';
 
-    std::cout << tm.elapsed() << std::endl;
+    d = boost::gregorian::day_clock::universal_day();
+    std::cout << d.year() << '\n';
+    std::cout << d.month() << '\n';
+    std::cout << d.day() << '\n';
+
+    d = boost::gregorian::date_from_iso_string("20140131");
+    std::cout << d.year() << '\n';
+    std::cout << d.month() << '\n';
+    std::cout << d.day() << '\n';
 
     return 0;
 }
